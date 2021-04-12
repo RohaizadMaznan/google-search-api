fc5d8cb1b545883f293a74207af4d72e371a071b
Rohaizad Maznan <rohaizadmaznan@gmail.com>
2021-04-13 01:06:12 +0800

    complete nextjs app

---
+++ keys.js
@@ -0,0 +1,5 @@
+// API Key - https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = 'AIzaSyBjZ7gayLdEbmh-bB_Oal0s5DLkvF9Z2n8'
+
+// Content Key - https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = 'bdb881512eeefe53a'
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,80 @@
+import { useRef } from 'react'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Avatar from '../components/Avatar'
+import { MicrophoneIcon, ViewGridIcon } from '@heroicons/react/solid'
+import { SearchIcon } from '@heroicons/react/outline'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+import Footer from '../components/Footer'
 
 export default function Home() {
+
+  const router = useRouter()
+  const searchInputRef = useRef(null)
+
+  const search = (e) => {
+    e.preventDefault()
+    const term = searchInputRef.current.value
+
+    if (!term) return;
+
+    router.push(`/search?term=${term}`)
+  }
+
   return (
-    <div className={styles.container}>
+    <div className="flex flex-col items-center justify-center h-screen" >
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Google</title>
+        <link rel="icon" href="https://expresswriters.com/wp-content/uploads/2015/09/google-new-logo-450x450.jpg" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      {/* Header */}
+      <header className="flex w-full p-5 justify-between text-sm text-gray-700">
+        {/* Left */}
+        <div className="flex space-x-4 items-center">
+          <p className="link">About</p>
+          <p className="link">Store</p>
         </div>
-      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        {/* Right */}
+        <div className="flex space-x-4 items-center">
+          <p className="link">Gmail</p>
+          <p className="link">Images</p>
+
+          {/* Icon */}
+          <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
+
+          {/* Avatar */}
+          <Avatar url="https://media-exp1.licdn.com/dms/image/C5103AQFE74YaU-0acA/profile-displayphoto-shrink_200_200/0/1521792249204?e=1622678400&v=beta&t=Cc5IXcaFqMd6wMtQa1qt5gEadWImnLlV6SkK5Dxdc5I" />
+        </div>
+      </header>
+
+      {/* Body */}
+      <form className="flex flex-col items-center mt-44 flex-grow w-4/5">
+        <Image 
+          src='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png'
+          height={100} width={300}
+        />
+
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl">
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input 
+            ref={searchInputRef} 
+            type="text" 
+            className="flex-grow focus:outline-none" />
+          <MicrophoneIcon className="h-5 ml-3 text-gray-500" />
+        </div>
+
+        <div className="flex flex-col space-y-2 justify-center mt-8 sm:flex-row sm:space-y-0 sm:space-x-4">
+          <button onClick={search} className="btn">Google Search</button>
+
+          <button className="btn">I'm Feeling Lucky</button>
+        </div>
+      </form>
+
+      {/* Footer */}
+      <Footer />
+
     </div>
   )
 }
